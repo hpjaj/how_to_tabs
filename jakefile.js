4f54b921115c9c7973c842fa41f223442e6804c3
hpjaj <hmlevine@gmail.com>
2015-09-29 17:44:53 -0600

    Used semver to comapre Node versions

--- jakefile.js
+++ jakefile.js
@@ -1,6 +1,8 @@
 (function() {
   "use strict";
 
+  var semver = require("semver");
+
   desc("Default build");
   task("default", [ "version" ], function() {
     console.log("\n\nBUILD OK");
@@ -11,10 +13,10 @@
     console.log("Checking Node version: .");
 
     var packageJson = require("./package.json");
-    var expectedVersion = "v" + packageJson.engines.node;
+    var expectedVersion = packageJson.engines.node;
 
     var actualVersion = process.version;
-    if (actualVersion !== expectedVersion) {
+    if (semver.neq(expectedVersion, actualVersion)) {
       fail("Incorrect Node version: expected " + expectedVersion + ", but was " + actualVersion);
     }
   });
