d62976ad9c3d4237299071ab6c1ca2d3f7fac92b
hpjaj <hmlevine@gmail.com>
2015-10-03 20:54:33 -0600

    JS gotchas part 1

--- jakefile.js
+++ jakefile.js
@@ -1,3 +1,5 @@
+/* globals desc: false, task: false, complete: false, fail: false */
+
 (function() {
   "use strict";
 
@@ -28,7 +30,23 @@
 
     jshint.checkFiles({
       files: "Jakefile.js",
-      options: {},
+      options: {
+        bitwise: true,
+        eqeqeq: true,
+        forin: true,
+        freeze: true,
+        futurehostile: true,
+        latedef: "nofunc",
+        noarg: true,
+        nocomma: true,
+        nonbsp: true,
+        nonew: true,
+        strict: true,
+        undef: true,
+
+        node: true,
+        browser: true
+      },
       globals: {}
     }, complete, fail);
   }, { async: true });
