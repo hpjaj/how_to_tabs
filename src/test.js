67823638401ccb00367303709f1b72c7c0596332
hpjaj <hmlevine@gmail.com>
2015-10-03 22:02:01 -0600

    Added floating point test

--- src/test.js
+++ src/test.js
@@ -3,10 +3,15 @@
 
   var assert = require("chai").assert;
 
+  // basic addtion
   assert.equal(add(3, 4), 7);
 
+  // IEEE 754 floating point
+  assert.equal(add(0.1, 0.2), 0.3);
+
+
   function add(a, b){
-    return a - b;
+    return a -  b;
   }
 
 }());
