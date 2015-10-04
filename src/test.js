4f10cd749403283c201e1fe46c30e56f2a99be39
hpjaj <hmlevine@gmail.com>
2015-10-03 22:30:42 -0600

    Installed Mocha for running tests

--- src/test.js
+++ src/test.js
@@ -3,15 +3,22 @@
 
   var assert = require("chai").assert;
 
-  // basic addtion
-  assert.equal(add(3, 4), 7);
+  describe("Addition", function() {
 
-  // IEEE 754 floating point
-  assert.equal(add(0.1, 0.2), 0.3);
+    it("adds positve numbers", function() {
+      assert.equal(add(3, 4), 7);
+    });
+
+    it("uses IEEE 754 floating point", function() {
+      assert.equal(add(0.1, 0.2), 0.30000000000000004);
+    });
+
+  });
+
 
 
   function add(a, b){
-    return a -  b;
+    return a + b;
   }
 
 }());
