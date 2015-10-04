0cc6f5cf35b67a9769cf579623d182d7c50fe8cd
hpjaj <hmlevine@gmail.com>
2015-10-04 10:05:32 -0600

    Prevent mocha globals from triggering lint errors

--- src/test.js
+++ src/test.js
@@ -6,12 +6,16 @@
   describe("Addition", function() {
 
     it("adds positve numbers", function() {
-      assert.equal(add(3, 4), 7);
+      assertEqual(add(3, 4), 7);
     });
 
     it("uses IEEE 754 floating point", function() {
-      assert.equal(add(0.1, 0.2), 0.30000000000000004);
+      assertEqual(add(0.1, 0.2), 0.30000000000000004);
     });
+
+    function assertEqual(actual, expected) {
+      if (actual !== expected) throw new Error("expected");
+    }
 
   });
 
